cc92ebd9b6c40703e4e7748d8f2c19a4fc5f0283
Sean <ssslim2002@gmail.com>
2023-07-03 16:12:43 -0700

    Initial Commit

---
+++ ingredients/ingredientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ingredientsApp.swift
+//  ingredients
+//
+//  Created by Sean Lim on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ingredientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
